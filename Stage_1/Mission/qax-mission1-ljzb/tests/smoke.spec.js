ec30f19a2bc0fc63248c307d91c6696bd22ac348
Luis Zuluaga <luiszuluaga90@gmail.com>
2026-04-21 21:30:29 -0500

    creación de tarjetas

--- Stage_1/Mission/qax-mission1-ljzb/tests/smoke.spec.js
+++ Stage_1/Mission/qax-mission1-ljzb/tests/smoke.spec.js
@@ -4,7 +4,9 @@ test.describe('Smoke Test - Homebanking', () => {
   const baseURL = 'https://homebanking-demo.onrender.com';
   let token;
   let userEmail;
+  let userAccount;
   let userPassword = 'mipassword123';
+
 
   test('Flujo crítico E2E', async ({request}) => {
     const timestamp = Date.now();
@@ -20,9 +22,9 @@ test.describe('Smoke Test - Homebanking', () => {
       username
     }
     });
-    expect(register.status()).toBe(200);  
+    expect(register.status()).toBe(200);
     //Fin de registro
-    
+
     //Inicio de login
     const login = await request.post(`${baseURL}/auth/login`, {
     data: {
@@ -30,9 +32,9 @@ test.describe('Smoke Test - Homebanking', () => {
       username
     }
     });
-    expect(login.status()).toBe(200); 
+    expect(login.status()).toBe(200);
     const loginBody = await login.json();
-    const token = loginBody.token;
+    const token = loginBody.token; //Se obtiene el token del usuario
     // console.log(token)
     //Fin de login
 
@@ -44,9 +46,22 @@ test.describe('Smoke Test - Homebanking', () => {
     });
     expect(dashboard.status()).toBe(200);
     const dashboardBody = await dashboard.json();
-    const accountId = dashboardBody.data.accounts[0].id;
+    const accountId = dashboardBody.data.accounts[0].id; //Se obtiene el id de la cuenta para agregarle una tarjeta
     // console.log(accountId);
-    //Fin de login
-   
+    //Fin de dashboard
+
+    // Inicio de creación de tarjeta
+    const tarjeta = await request.post(`${baseURL}/tarjetas`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    data: {
+      id_cuenta_asociada: accountId,
+      marca: "Mastercard",
+      tipo: "Crédito"
+    }
+    });
+  //  Fin de creación de tarjeta
+
   });
 });
